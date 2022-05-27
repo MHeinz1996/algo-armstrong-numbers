b68ac01187bd5722860777abe3b156ae488b6058
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-27 00:04:17 -0500

    Completed algo

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,19 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+exports.findArmstrongNumbers = function(numbers) {
+    let armstrongNumbers = [];
 
+    for(let i=0; i<numbers.length; i++) {
+        let digits = String(i).split('');
+        let sum = 0;
+        for(let i=0; i<digits.length; i++) {
+            sum += Math.pow(digits[i], digits.length)
+        }
+
+        if(sum == numbers[i]) {
+            armstrongNumbers.push(numbers[i]);
+        }
+    }
+
+    return armstrongNumbers;
 };
